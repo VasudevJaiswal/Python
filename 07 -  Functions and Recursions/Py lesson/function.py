e0f11bbc7f4b9a7d5cd5e4f014d80b79e5abce9e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-09 13:52:22 -0700

    Python.py

---
+++ 07 -  Functions and Recursions/Py lesson/function.py
@@ -0,0 +1,31 @@
+# Chapter 8 – Functions and Recursions
+
+# A function is a group of statements performing a specific task.
+
+# When a program gets bigger in size and its complexity grows, it gets difficult for a programmer to keep track of which piece of code is doing what!
+
+# A function can be reused by the programmer in a given program any number of times.
+
+
+# Percentage finding Using Function 
+
+# 01.
+
+marks = [45,78,86,77]
+Percentage = (sum(marks)/400)*100
+print(Percentage)
+
+# 02. 
+marks1 = [75,98,88,78]
+p2 = (sum(marks1)/400)*100
+print(p2)
+
+# If programmer don't know function then he use to find percentage this method
+
+# 03. 
+marks2 = [75,98,88,78]
+p2 = ((marks2[0]+marks2[1]+marks2[2]+marks2[3])/400)*100
+print(p2)
+
+
+
