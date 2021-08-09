4ca6e5a2edd985187238573ea1987e74e533a33c
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-09 14:14:20 -0700

    Python.py

---
+++ 08 -  Functions and Recursions/Py lesson/Built_in_fun.py
@@ -0,0 +1,9 @@
+# Types of functions in Python
+# There are two types of functions in Python:
+
+# Built-in functions #Already present in Python
+# User-defined functions #Defined by the user
+
+# Examples of built-in function includes len(), print(), range(), etc.
+
+# The func1() function we defined is an example of a user-defined function.
