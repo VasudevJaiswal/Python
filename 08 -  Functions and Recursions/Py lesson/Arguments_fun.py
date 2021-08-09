8f478ca44a75010fe10e52729fcdf98f2332e129
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-09 14:23:30 -0700

    Python.py

--- 08 -  Functions and Recursions/Py lesson/Arguments_fun.py
+++ 08 -  Functions and Recursions/Py lesson/Arguments_fun.py
@@ -1,4 +1,4 @@
 # Functions with arguments
 
 ''' A function can accept some values it can work with. 
-# We can put these values in the parenthesis. A function can also return values as shown below:
+ We can put these values in the parenthesis. A function can also return values as shown below: '''
