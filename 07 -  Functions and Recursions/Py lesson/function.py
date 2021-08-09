4d9f1dfad1a3d4cf08dca7a9b5efc801686c1ebd
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-09 14:00:34 -0700

    Python.py

--- 07 -  Functions and Recursions/Py lesson/function.py
+++ 07 -  Functions and Recursions/Py lesson/function.py
@@ -35,6 +35,19 @@ def percent(marks):
     return p
 
 marks1 = [45,78,86,77]
-percentage1 = percent(marks)    # I call the function - percent
+percentage1 = percent(marks1)    # We call the function - percent
 
 marks2 = [75,98,88,78]
+percentage2 = percent(marks2)     # We call the function - percent
+
+
+
+print(percentage1,percentage2)
+
+
+# result of the function 
+# 71.5 84.75  
+
+# That's very easy way to call a  funtion to evalute a program 
+
+
