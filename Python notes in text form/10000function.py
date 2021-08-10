ef3a1ef0c99a92641a248a9f803c0fa204b4acab
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-10 21:05:37 +0530

    Python.py

---
+++ Python notes in text form/10000function.py
@@ -0,0 +1,36 @@
+# What is Function ?
+'''Function is a Block of Code or set of instructions assigned for a particular task'''
+
+# Why fuction?
+
+''' ** Function reduces code redundancy 
+    ** function consumes less memory   
+    ** Function Reduces size of program  
+'''
+
+# Types of Fuction's
+'''
+* Library Functions(Built-in-Funciton) --- input(), range(),count()
+* User defined Function - 
+  def keywrod is used  to create a function
+@Syntax 
+    def fun()
+        instructions
+'''
+
+# Example
+
+def x():
+    print("Hii I'm Vasudev Jaiswal")
+    print("Good ! Morning bro")
+
+def y():
+    a = (int)(input("Enter first value: "))
+    b = (int)(input("Enter Seconed Value: "))
+    c = a+b
+    print("Addition is = " c)
+
+
+x()  #Call to fuction using user defined function
+# y()
+# x()
