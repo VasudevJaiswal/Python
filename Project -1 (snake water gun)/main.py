0edc431d59667e9640b92591a8ef07918ae48466
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-10 19:45:14 +0530

    Python.py

---
+++ Project -1 (snake water gun)/main.py
@@ -0,0 +1,15 @@
+# Project 1: Snake, Water, Gun Game
+# We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a Python program capable of playing this game with the user.
+
+import random
+
+def game():
+    pass
+
+ randint = (1,3)
+ ranNo = random.randint
+ print(ranNo)
+# a = input("Computer Turn: Snake(s) Water (w) or Gun (g) ? ")
+# b = input("Player;s Turn: Snake(s) Water (w) or Gun (g) ? ")
+
+
