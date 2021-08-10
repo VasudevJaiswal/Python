ceb9a245311897ea35078fda617dbe4df9bf43d2
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-10 19:16:24 +0530

    Python.py

--- 08 -  Functions and Recursions/Practice set/04.py
+++ 08 -  Functions and Recursions/Practice set/04.py
@@ -1,5 +1,6 @@
 '''Problem 4. How do you prevent a python print() function to print a new line at the end?'''
-print("Hello")
-print("How")
-print("are")
-print("you")
+
+print("Hello", end=" ")
+print("How",end= " ")
+print("are",end= " ")
+print("you",end= " ")
---
+++ 08 -  Functions and Recursions/Practice set/05.py
@@ -0,0 +1,4 @@
+'''Problem 5. Write a recursive function to calculate the sum of first n natural numbers.'''
+
+n! = (n-1)! *n
+sum(n) = sum(n-1) +  n
