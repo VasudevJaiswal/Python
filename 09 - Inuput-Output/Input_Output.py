47037d4c12c3e8764de4ecac3ffae0f75e0d55f8
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-11 20:09:00 +0530

    Python.py

---
+++ 09 - Inuput-Output/Input_Output.py
@@ -0,0 +1,9 @@
+# Chapter 9 – File I/O
+
+# The random access memory is volatile, and all its contents are lost once a program terminates.
+
+# In order to persist the data forever, we use files.
+
+''' A file is data stored in a storage device. A python program can talk to the file by reading content
+ from it and writing content to it. '''
+
