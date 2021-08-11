f2f098b9b42dae5a5e8b0997f344359bf0811526
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-11 20:29:30 +0530

    Python.py

---
+++ 09 - Inuput-Output/othres_Method.py
@@ -0,0 +1,18 @@
+# Other methods to read the file
+
+# We can also use f.readline() function to read one full line at a time.
+
+# f.readline()               #Reads one line from the file
+# Modes of opening a file
+
+# r – open for reading
+
+# w – open for writing
+
+# a – open for appending
+
+# + -> open for updating
+
+# ‘rb’ will open for read in binary mode
+
+# ‘rt’ will open for read in text mode
