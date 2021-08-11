b3b2f6f540105917c18e7f04981d35ca9822629e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-11 20:30:31 +0530

    Python.py

---
+++ 09 - Inuput-Output/Write_files.py
@@ -0,0 +1,9 @@
+# Writing Files in Python
+
+# In order to write to a file, we first open it in write or append mode, after which, we use the python’s f.write() method to write to the file!
+
+# f = open(“this.txt”, “w”)
+
+# f.write(“This is nice”)        #Can be called multiple times
+
+# f.close()
